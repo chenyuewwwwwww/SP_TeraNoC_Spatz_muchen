9e921a41c36a91486b6346ee86623fad5378c8e1
sem24f14 Chenyue Wang (chenwang) and Ho Tin Hung (hohung) <sem24f14@pisoc4.ee.ethz.ch>
2025-11-11 13:34:25 +0100

    teranoc-spatz new kernal and config

---
+++ config/config.mk
@@ -0,0 +1,93 @@
+# Copyright 2021 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+# Author: Samuel Riedel, ETH Zurich
+#         Matheus Cavalcante, ETH Zurich
+
+######################
+##  MemPool flavor  ##
+######################
+
+# Choose a MemPool flavor, either "minpool" or "mempool".
+# Check the README for more details
+ifndef config
+  ifdef MEMPOOL_CONFIGURATION
+    config := $(MEMPOOL_CONFIGURATION)
+  else
+    # Default configuration, if neither `config` nor `MEMPOOL_CONFIGURATION` was found
+    config := mempool
+  endif
+endif
+include $(MEMPOOL_DIR)/config/$(config).mk
+
+#############################
+##  Address configuration  ##
+#############################
+
+# Boot address (in dec)
+boot_addr ?= 2684354560 # A0000000
+
+# L2 memory configuration (in dec)
+l2_base  ?= 2147483648 # 80000000
+
+# L1 size per bank (in dec)
+l1_bank_size ?= 1024
+
+# Size of sequential memory per core (in bytes)
+# (must be a power of two)
+seq_mem_size ?= 512
+
+# Size of stack in sequential memory per core (in bytes)
+stack_size ?= 512
+
+#########################
+##  AXI configuration  ##
+#########################
+# AXI bus data width (in bits)
+axi_data_width ?= 512
+
+# Read-only cache line width in AXI interconnect (in bits)
+ro_line_width ?= 512
+
+#############################
+##  Xqueues configuration  ##
+#############################
+
+# XQueue extension's queue size in each memory bank (in words)
+xqueue_size ?= 0
+
+################################
+##  Optional functionalities  ##
+################################
+
+# Enable the XpulpIMG extension, same as mempool-spatz, here defalut as disabled
+xpulpimg ?= 0
+
+# Enable FPU extensions, same as mempool-spatz, here defalut as disabled
+zfinx ?= 0
+
+# Enable FPU extensions
+zquarterinx ?= 0
+
+# DivSqrt deactivated by default
+xDivSqrt ?= 0
+
+# DRAMsys co-simulation: dram/sram
+l2_sim_type ?= sram
+axi_width_interleaved ?= 16
+
+# Enable SPM bank id remapping inside of each tile
+spm_bank_id_remap ?= 0
+
+# Enable tile id remapping inside of each group
+tile_id_remap ?= 0
+
+# Enable the spm access pattern profiling
+spm_profiling ?= 0
+
+# Enable the interconnect access pattern profiling
+noc_profiling ?= 0
+
+
+
---
+++ config/test_4x4noc_128core.mk
@@ -0,0 +1,109 @@
+# Copyright 2025 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Author: Yichao Zhang, ETH Zurich
+
+###########################
+## 1. Architecture Config
+###########################
+
+# Global Control
+terapool ?= 1
+
+# Number of cores
+num_cores ?= 128
+
+# Number of groups
+num_groups ?= 16
+
+# Number of cores per MemPool tile
+num_cores_per_tile ?= 4
+
+# L1 scratchpad banking factor
+banking_factor ?= 8
+
+# Number of shared divsqrt units per MemPool tile
+# Defaults to 1 if xDivSqrt is activated
+num_divsqrt_per_tile ?= 1
+
+#####################
+## 2. NoC Config
+#####################
+
+# FlooNoC configuration
+num_directions ?= 5
+num_x          ?= 4
+
+# Topology
+# 0: 2D mesh, 1: torus
+noc_topology ?= 0
+
+# Routing algorithm
+# 0: xy, 1: odd-even, 2: o1
+noc_routing_algorithm ?= 0
+
+# NoC remapping configuration
+# 0: no remapping, 1: req remapping, 2: resp remapping 3: req+resp remapping
+noc_router_remapping ?= 0
+
+# Virtual channel number
+noc_virtual_channel_num ?= 1
+
+# Channel configuration mode (internal control only)
+# Options: baseline, narrow, enhanced
+channel_config_mode := baseline  # Current MinPool setting
+
+# Channel configuration based on selected mode
+ifeq ($(strip $(channel_config_mode)), baseline)
+noc_req_rd_channel_num   ?= 0
+noc_req_rdwr_channel_num ?= 2
+noc_req_wr_channel_num   ?= 0
+noc_resp_channel_num     ?= 2
+
+else ifeq ($(strip $(channel_config_mode)), narrow)
+noc_req_rd_channel_num   ?= 1
+noc_req_rdwr_channel_num ?= 1
+noc_req_wr_channel_num   ?= 0
+noc_resp_channel_num     ?= 2
+
+else ifeq ($(strip $(channel_config_mode)), enhanced)
+noc_req_rd_channel_num   ?= 1
+noc_req_rdwr_channel_num ?= 1
+noc_req_wr_channel_num   ?= 0
+noc_resp_channel_num     ?= 3
+
+else
+$(error Unsupported channel_config_mode: $(channel_config_mode))
+endif
+
+# Print configuration info for debugging
+$(info [DEBUG:][noc_req_rd_channel_num]----$(noc_req_rd_channel_num))
+$(info [DEBUG:][noc_req_rdwr_channel_num]--$(noc_req_rdwr_channel_num))
+$(info [DEBUG:][noc_req_wr_channel_num]----$(noc_req_wr_channel_num))
+$(info [DEBUG:][noc_resp_channel_num]------$(noc_resp_channel_num))
+
+# Router buffer configuration
+noc_router_input_fifo_dep  ?= 2
+noc_router_output_fifo_dep ?= 2
+
+# Router remapping configuration
+noc_router_remap_group_size ?= 2
+
+###########################
+## 3. AXI and DMA Config
+###########################
+
+# Radix for hierarchical AXI interconnect
+# Typically: num_cores_per_tile * num_tiles_per_group + num_dma_per_groups
+axi_hier_radix ?= 9
+
+# Number of AXI masters per group
+axi_masters_per_group ?= 1
+
+# Number of DMA backends in each group
+dmas_per_group ?= 1  # Burst Length = 16
+
+# L2 Banks/Channels
+l2_size  ?= 16777216  # 1000000
+l2_banks ?= 16
